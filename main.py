5ec73795ac84197828843e6cc0dba214a70a8467
Tarun Ravada <tarunravada@gmail.com>
2022-12-05 08:41:01 -0500

    updates app

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import altair as alt
-import numpy as np
 from pycaret.classification import *
 
 # Use the full page instead of a narrow central column
